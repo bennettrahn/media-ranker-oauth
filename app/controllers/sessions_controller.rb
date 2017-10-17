dcaf12840b436decf01e198d9094b0994deb667c
Bennett Rahn <rahnibennett@gmail.com>
2017-10-16 19:20:07 -0700

    move the login out of user and into session_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,34 +1,45 @@
 class SessionsController < ApplicationController
-  def login_form
-  end
+  skip_before_action :require_login, only: [:login]
 
   def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      user = User.find_by(provider: params[:provider], uid: auth_hash['uid'])
+
+      if user.nil?
+        user = User.from_auth_hash(params[:provider], auth_hash)
+
+        if user.save
+          session[:user_id] = user.id
+          flash[:status] = :success
+          flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+        else
+          flash.now[:status] = :failure
+          flash.now[:result_text] = "Could not log in"
+          flash.now[:messages] = user.errors.messages
+        end
+
       else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash[:status] = :success
+        flash[:result_text] = "Successfully logged in as returning user #{user.name}"
       end
+
+      session[:user_id] = user.id
+
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create user from OAuth data"
     end
+
     redirect_to root_path
   end
 
   def logout
     session[:user_id] = nil
-    flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
     redirect_to root_path
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:login]
+
 
   def index
     @users = User.all
@@ -10,45 +10,4 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
-  def login
-
-    auth_hash = request.env['omniauth.auth']
-
-    if auth_hash['uid']
-      user = User.find_by(provider: params[:provider], uid: auth_hash['uid'])
-
-      if user.nil?
-        user = User.from_auth_hash(params[:provider], auth_hash)
-
-        if user.save
-          session[:user_id] = user.id
-          flash[:status] = :success
-          flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
-        else
-          flash.now[:status] = :failure
-          flash.now[:result_text] = "Could not log in"
-          flash.now[:messages] = user.errors.messages
-        end
-
-      else
-        flash[:status] = :success
-        flash[:result_text] = "Successfully logged in as returning user #{user.name}"
-      end
-
-      session[:user_id] = user.id
-
-    else
-      flash[:status] = :failure
-      flash[:result_text] = "Could not create user from OAuth data"
-    end
-
-    redirect_to root_path
-  end
-
-  def logout
-    session[:user_id] = nil
-    redirect_to root_path
-    flash[:status] = :success
-    flash[:message] = "Successfully logged out"
-  end
 end
